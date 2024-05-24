e8fa8ac0717c95a97d0422fa0c3d5b2b87a2473e
Ponkrshnan Thiagarajan <54912764+ponkrshnan@users.noreply.github.com>
2024-05-24 12:24:48 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 pip install torch torchinfo beartype pyDOE
 git clone -b feature/scientific_machine_learning https://github.com/SURGroup/UQpy.git
 cd UQpy
+git reset --hard dab40c0
 python setup.py 4.1.5 install
 cd ..
 yes | rm -r UQpy
